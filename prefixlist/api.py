2a02c82898df510d74f2b78d121d7bee891d88a2
Eric Lindsjö <eric.lindsjo@glesys.se>
2016-12-26 10:31:47 +0100

    Initial version of the API

---
+++ prefixlist/api.py
@@ -0,0 +1,43 @@
+from flask import Flask
+from flask_restful import reqparse, abort, Api, Resource
+
+app = Flask("pre-fixlist")
+api = Api(app)
+
+class PrefixListList(Resource):
+
+    def get(self):
+        # Return a list of all prefix lists we know about
+        return None
+
+
+class PrefixList(Resource):
+
+    def get(self, listid):
+        # Return a list of versions 
+        return None
+
+    def post(self):
+        # Create a new prefixlist
+        return None
+
+    def put(self, listid):
+        # Update a prefixlist
+        return None
+
+    def delete(self, listid):
+        return None
+
+class PrefixListVersion(Resource):
+
+    def get(self, listid, version):
+        # Return a list of prefixes contained in the specific version of the prefixlist
+        return None
+
+
+api.add_resource(PrefixListList, "/prefixlist")
+api.add_resource(PrefixList, "/prefixlist/<int:listid>")
+api.add_resource(PrefixListVersion, "/prefixlist/<int:listid>/<string:version>")
+
+if __name__ == '__main__':
+    app.run(debug=True)
